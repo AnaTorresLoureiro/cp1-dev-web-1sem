2e96ebbda4b521fc3e7f5ded9317bb4f503ecdd2
Ana Laura T.L <al.t.loureiro0@gmail.com>
2024-03-28 15:31:54 -0300

    Primeiro exercício-definição de variáveis em JS

---
+++ script.js
@@ -0,0 +1,14 @@
+//1
+function dataTypes(){
+    //indefinido
+    let aula;
+    console.log(aula)
+
+    //vazio
+    let disciplina = null
+    console.log(disciplina)
+
+    //com valor
+    let aluno="Fiaper"
+    console.log(aluno)
+}
